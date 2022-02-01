1710941c42774acbe5e6951287f1e3435722950b
leena-Eid <lynaa388@gmail.com>
2022-02-01 02:07:16 +0200

    The main files

---
+++ js/sara.js
@@ -0,0 +1,136 @@
+// window.onload = alert('wellcome to Animal adoption page');
+
+var theButton = document.getElementsByTagName('button');
+console.log(theButton);
+var  a = theButton[0];
+var b = theButton[1];
+var c = theButton[2];
+var d = theButton[3];
+
+var tt = 'put your phone numer please , we will contact you soon';
+a.onclick = function firstOne(){
+    'use strict';
+    return prompt(tt);
+}
+b.onclick = function firstOne(){
+    'use strict';
+    return prompt(tt);
+}
+c.onclick = function firstOne(){
+    'use strict';
+    return prompt(tt);
+}
+d.onclick = function firstOne(){
+    'use strict';
+    return prompt(tt);
+}
+
+
+
+
+//the stack over flow code
+/*
+var greeting = "What is your name?"; 
+
+function getUserName() {
+    return prompt(greeting);
+}
+
+function yourFunction() {
+     alert("Hello "+getUserName());
+}
+
+yourFunction();
+/*
+if(u.value>=20){
+    prompt("It's ok , please write your number , we will contact you soon ");
+}elseif( isNaN( u.value)){"please put number!"}
+else {
+    " We are sorry , your age dont allow you to adopt an animal"
+}
+
+/*
+var divs = document.getElementsByClassName('box');
+var A = divs[0];
+var B = divs[1];
+var C = divs[2];
+var D = divs[3];
+var ageInput0 = document.createElement('input');
+var ageInput1 = document.createElement('input');
+var ageInput2 = document.createElement('input');
+var ageInput3 = document.createElement('input');
+var tn0 =document.createTextNode( ' write your age please : ');
+var tn1 =document.createTextNode("write your age please : ");
+var tn2 =document.createTextNode("write your age please : ");
+var tn3 =document.createTextNode("write your age please : ");
+var s0 = document.createElement('button')
+var buttonTextN = createTextNode('ok')
+A.appendChild(s0);
+s0.appendChild(buttonTextN);
+
+
+A.appendChild(tn0);
+B.appendChild(tn1);
+C.appendChild(tn2);
+D.appendChild(tn3);
+A.appendChild(ageInput0);
+B.appendChild(ageInput1);
+C.appendChild(ageInput2);
+D.appendChild(ageInput3);
+ageInput0.style.width = '100px' ;
+ageInput0.style.height ='30px' ;
+ageInput0.style.borderBottom = '3px #5eaaa8 solid' ;
+ageInput0.style.borderRight = '3px #5eaaa8 solid' ;
+ageInput0.style.backgroundColor = 'none' ;
+ageInput0.style.borderRadius = '7px' ;
+ageInput1.style.width = '100px' ;
+ageInput1.style.height ='30px' ;
+ageInput1.style.borderBottom = '3px #5eaaa8 solid' ;
+ageInput1.style.borderRight = '3px #5eaaa8 solid' ;
+ageInput1.style.backgroundColor = 'none' ;
+ageInput1.style.borderRadius = '7px' ;
+ageInput2.style.width = '100px' ;
+ageInput2.style.height ='30px' ;
+ageInput2.style.borderBottom = '3px #5eaaa8 solid' ;
+ageInput2.style.borderRight = '3px #5eaaa8 solid' ;
+ageInput2.style.backgroundColor = 'none' ;
+ageInput2.style.borderRadius = '7px' ;
+ageInput3.style.width = '100px' ;
+ageInput3.style.height ='30px' ;
+ageInput3.style.borderBottom = '3px #5eaaa8 solid' ;
+ageInput3.style.borderRight = '3px #5eaaa8 solid' ;
+ageInput3.style.backgroundColor = 'none' ;
+ageInput3.style.borderRadius = '7px' ;
+tn0.style.fontWeight = '200';
+if(ageInput0.value >= 20){
+    prompt('please write your number , we will contact you soon');
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
